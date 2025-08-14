0f0f8812d6ae8b5467011824973988111cb80028
sami6720 <saminur67@gmail.com>
2025-08-14 18:27:04 -0400

    Update QPerm to allow soft-moe

--- purejaxql/perm_pqn_craftax.py
+++ purejaxql/perm_pqn_craftax.py
@@ -39,10 +39,20 @@ class QNetworkPerm(nn.Module):
 
     @nn.compact
     def __call__(self, x: jnp.ndarray, train: bool):
+
+
+        if self.norm_type == "layer_norm":
+            def normalize(x): return nn.LayerNorm()(x)
+        elif self.norm_type == "batch_norm":
+            def normalize(x): return BatchRenorm(
+                use_running_average=not train)(x)
+        else:
+            def normalize(x): return x
+
         if "Pixels" in self.config["ENV_NAME"]:
             B, H, W, C = x.shape
 
-            if self.config["USE_CONV_FOR_PIXELS"]:
+            if self.config["FEATURES_FROM_PIXELS_STRAT"] == 'conv':
                 initializer = nn.initializers.xavier_uniform()
 
                 x = x.astype(jnp.float32) / 255.0
@@ -58,10 +68,63 @@ class QNetworkPerm(nn.Module):
                     features=64, kernel_size=(3, 3), strides=(1, 1), kernel_init=initializer
                 )(x)
                 x = nn.relu(x)
+            elif self.config["FEATURES_FROM_PIXELS_STRAT"] == 'scaled_pixels':
+                x = x.astype(jnp.uint32) / 255.0
+            elif self.config["FEATURES_FROM_PIXELS_STRAT"] == 'flattened':
+                x = x.reshape(B, -1)
+            elif self.config["FEATURES_FROM_PIXELS_STRAT"] == 'scaled_flattened':
+                x = x.astype(jnp.uint32) / 255.0
                 x = x.reshape(B, -1)
             else:
-                x = x.reshape(B, -1)
+                raise ValueError("Wrong way to generate feature from pixels")
 
+
+            if self.config['USE_SOFT_MOE_MULTI_EXPERT']:
+
+                assert 'flattened' not in self.config["FEATURES_FROM_PIXELS_STRAT"]
+
+                B, H, W, D = x.shape
+                print("Debug print x.shape after conv", x.shape)
+                #TOKENIZE PerConv
+                x = x.reshape(B, -1, D) # Shape (H*W) X D
+                # Let M = H * W
+                NUM_SLOTS_PER_EXPERT = (H * W) // self.config["NUM_EXPERTS"] # Each expert sort of gets equal number of tokens
+                phi = self.param("phi", nn.initializers.normal(), (D, self.config["NUM_EXPERTS"], NUM_SLOTS_PER_EXPERT)) # Shape: DNP
+                logits = jnp.einsum("bmd,dnp->bmnp", x, phi)
+
+                dispatch = jax.nn.softmax(logits, axis=1)
+                combine_per_expert = jax.nn.softmax(logits, axis=-1)
+
+                x_tilda = jnp.einsum("bmd,bmnp->bnpd", x, dispatch)
+
+                stack = []
+                for i in range(self.config["NUM_EXPERTS"]):
+                    expert_out = x_tilda[:, i, :, :]
+                    for i in range(self.num_layers):
+                        expert_out = nn.Dense(D)(expert_out)
+                        expert_out = normalize(expert_out)
+                        expert_out = nn.relu(expert_out)
+                    stack.append(expert_out)
+                y_tilda = jnp.stack(stack, axis=1) # Shape: BNPD
+
+                stack = []
+                y_tilda_tilda = jnp.einsum('bnpd,bmnp->bnmd', y_tilda, combine_per_expert)
+                # Output layer: one Q-value per action per expert
+                for i in range(self.config["NUM_EXPERTS"]):
+                    # The input is a vector of shape M * D into the Q-value head
+                    expert_perm_q_val = nn.Dense(self.action_dim)(y_tilda_tilda[:, i, :, :].reshape(B, -1))
+                    stack.append(expert_perm_q_val)
+                y = jnp.stack(stack, axis=1) # BNA
+
+                if self.config["EXPERT_OUTPUT_COMBINE_STRAT"] == 'sum':
+                    x = jnp.sum(y, axis=1)
+                    return x
+                elif self.config["EXPERT_OUTPUT_COMBINE_STRAT"] == "softmax_over_n_meanpool":
+                    combine_q_vectors = jax.nn.softmax(jnp.mean(logits, axis=(1, 3)), axis=1) # BN
+                    x = jnp.einsum("bn,bna->ba", combine_q_vectors, y)
+                    return x
+                else:
+                    raise ValueError("Incorrect EXPERT_OUTPUT_COMBINE_STRAT")
             # dummy normalize input for global compatibility
             x_dummy = BatchRenorm(use_running_average=not train)(x)
         else:
@@ -70,14 +133,6 @@ class QNetworkPerm(nn.Module):
             else:
                 # dummy normalize input for global compatibility
                 x_dummy = BatchRenorm(use_running_average=not train)(x)
-
-        if self.norm_type == "layer_norm":
-            def normalize(x): return nn.LayerNorm()(x)
-        elif self.norm_type == "batch_norm":
-            def normalize(x): return BatchRenorm(
-                use_running_average=not train)(x)
-        else:
-            def normalize(x): return x
 
         for l in range(self.num_layers):
             x = nn.Dense(self.hidden_size)(x)
